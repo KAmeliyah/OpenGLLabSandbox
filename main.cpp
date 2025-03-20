da55e9a6f7915545760a5a8cf282a8db79363c1b
Khadijah <ameliyah2005@gmail.com>
2025-03-20 14:06:58 +0000

    modify git ignore

--- main.cpp
+++ main.cpp
@@ -38,19 +38,6 @@ int main()
 	}
 
 
-	
-
-	//int w = 0;
-	//int h = 0;
-
-	//unsigned char* data = stbi_load("batman.png", &w, &h, NULL, 4);
-
-	//if (!data)
-	//{
-	//	throw std::exception();
-	//}
-
-
 	//can't init glew without connection to graphics card
 	//Glew loads OpenGL and extensions at runtime
 
@@ -65,141 +52,133 @@ int main()
 
 	float angle = 0;
 
-	//Prepare VBO
+	////Prepare VBO
 
-	// VBO is a memory buffer in the high speed memomry of the GPU. It holds information about the vertices
+	//// VBO is a memory buffer in the high speed memomry of the GPU. It holds information about the vertices
 
-	//triangle points - OpenGL vertices go counter clockwise
-	//This doesn't need to include z since it's 2D but it does here
-	const GLfloat positions[] = {
-		0.0f, 0.5f, 0.0f,
-		-0.5f, 0.5f, 0.0f,
-		-0.5f, 0.0f, 0.0f,
-		-0.5f, 0.0f, 0.0f,
-		0.0f, 0.0f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
-
-
-	//define the colours for the points of the triangle
-	const GLfloat colors[] = {
-		1.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		/*1.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,*/
-
-	};
-
-	const GLfloat texCoords[] = {
-		1.0f,0.0f,
-		0.0f,0.0f,
-		0.0f,1.0f,
-		0.0f,1.0f,
-		1.0f,1.0f,
-		1.0f,0.0f,
-	};
-
-	
-	//handle to reference the VBO
-	GLuint positionsVboId = 0;
-
-	//Create a new Vertex Buffer Object on the GPU and bind it
-	//Buffer Objects - store an array of unformatted memory allocated by the OpenGL context (GPU)
-	//glGenBufferes craetes the buffer object
-	glGenBuffers(1, &positionsVboId);
-
-	if (!positionsVboId)
-	{
-		throw std::runtime_error("Couldn't bind the VBO");
-	}
+	////triangle points - OpenGL vertices go counter clockwise
+	////This doesn't need to include z since it's 2D but it does here
+	//const GLfloat positions[] = {
+	//	0.0f, 0.5f, 0.0f,
+	//	-0.5f, 0.5f, 0.0f,
+	//	-0.5f, 0.0f, 0.0f,
+	//	-0.5f, 0.0f, 0.0f,
+	//	0.0f, 0.0f, 0.0f,
+	//	0.0f, 0.5f, 0.0f
+	//};
 
 
-	//This tells the GL_ARRAY_BUFFER that any data it gets is going to be copied into positionVboId 
-	glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
+	////define the colours for the points of the triangle
+	//const GLfloat colors[] = {
+	//	1.0f, 1.0f, 0.0f, 1.0f,
+	//	0.0f, 1.0f, 0.0f, 1.0f,
+	//	0.0f, 0.0f, 1.0f, 1.0f,
+	//	/*1.0f, 1.0f, 0.0f, 1.0f,
+	//	0.0f, 1.0f, 0.0f, 1.0f,
+	//	0.0f, 0.0f, 1.0f, 1.0f,*/
 
-	// Upload a copy of the data from memory into the new VBO
-	//GL_STATIC_DRAW - static memory on the graphics card
-	//GL_ARRAY_BUFFER is given the positions so it copies the vertex data into the VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+	//};
+
+	//const GLfloat texCoords[] = {
+	//	1.0f,0.0f,
+	//	0.0f,0.0f,
+	//	0.0f,1.0f,
+	//	0.0f,1.0f,
+	//	1.0f,1.0f,
+	//	1.0f,0.0f,
+	//};
+
+	//
+	////handle to reference the VBO
+	//GLuint positionsVboId = 0;
+
+	////Create a new Vertex Buffer Object on the GPU and bind it
+	////Buffer Objects - store an array of unformatted memory allocated by the OpenGL context (GPU)
+	////glGenBufferes craetes the buffer object
+	//glGenBuffers(1, &positionsVboId);
+
+	//if (!positionsVboId)
+	//{
+	//	throw std::runtime_error("Couldn't bind the VBO");
+	//}
 
 
+	////This tells the GL_ARRAY_BUFFER that any data it gets is going to be copied into positionVboId 
+	//glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
 
-
-	GLuint colorsVboId = 0;
-
-	//Create a colours VBO on the GPU and bind it
-
-	glGenBuffers(1, &colorsVboId);
-
-	if (!colorsVboId)
-	{
-		throw std::runtime_error("Couldn't bind the colours VBO");
-	}
-
-	glBindBuffer(GL_ARRAY_BUFFER, colorsVboId);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
-
-
-
-	// Reset the state
-	//Clear GL_ARRAY_BUFFER since it doesn't need the positions data anymore
-	//since positionsVboId has it now
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-
-	
+	//// Upload a copy of the data from memory into the new VBO
+	////GL_STATIC_DRAW - static memory on the graphics card
+	////GL_ARRAY_BUFFER is given the positions so it copies the vertex data into the VBO
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 
 
 
 
+	//GLuint colorsVboId = 0;
 
-	//Prepare VAO
-	//VAOs contain VBOs. Designed to store info about the complete rendered object
-	//So it stores each VBO(vertex) of the triangle
+	////Create a colours VBO on the GPU and bind it
 
-	//handle to reference the VAO
-	GLuint vaoId = 0;
+	//glGenBuffers(1, &colorsVboId);
 
-	// Create a new Vertex Array Object on the GPU and assign it to vaoId
-	glGenVertexArrays(1, &vaoId);
+	//if (!colorsVboId)
+	//{
+	//	throw std::runtime_error("Couldn't bind the colours VBO");
+	//}
 
-	if (!vaoId)
-	{
-		throw std::runtime_error("Couldn't bind the VAO");
-	}
+	//glBindBuffer(GL_ARRAY_BUFFER, colorsVboId);
 
-	//Bind the VAO as the current used object - This object is going to be drawn
-	glBindVertexArray(vaoId);
-
-	// Bind the position VBO, assign it to position 0 on the bound VAO
-	// and flag it to be used
-	glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
 
 
-	//Specify how the cooridinate data goes into attribute index 0 and has 3 floats per vertex
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
 
-	//Enable attribute index 0 as being used
-	glEnableVertexAttribArray(0);
-
-
-	//Bind the color VBO and assign it to position 1 on the Bound VAO
-	glBindBuffer(GL_ARRAY_BUFFER, colorsVboId);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GL_FLOAT), (void*)0);
-	glEnableVertexAttribArray(1);
+	//// Reset the state
+	////Clear GL_ARRAY_BUFFER since it doesn't need the positions data anymore
+	////since positionsVboId has it now
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 
-	// Reset the state
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	////Prepare VAO
+	////VAOs contain VBOs. Designed to store info about the complete rendered object
+	////So it stores each VBO(vertex) of the triangle
+
+	////handle to reference the VAO
+	//GLuint vaoId = 0;
+
+	//// Create a new Vertex Array Object on the GPU and assign it to vaoId
+	//glGenVertexArrays(1, &vaoId);
+
+	//if (!vaoId)
+	//{
+	//	throw std::runtime_error("Couldn't bind the VAO");
+	//}
+
+	////Bind the VAO as the current used object - This object is going to be drawn
+	//glBindVertexArray(vaoId);
+
+	//// Bind the position VBO, assign it to position 0 on the bound VAO
+	//// and flag it to be used
+	//glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
+
+
+	////Specify how the cooridinate data goes into attribute index 0 and has 3 floats per vertex
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+
+	////Enable attribute index 0 as being used
+	//glEnableVertexAttribArray(0);
+
+
+	////Bind the color VBO and assign it to position 1 on the Bound VAO
+	//glBindBuffer(GL_ARRAY_BUFFER, colorsVboId);
+	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GL_FLOAT), (void*)0);
+	//glEnableVertexAttribArray(1);
+
+
+	//// Reset the state
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glBindVertexArray(0);
 
 	ShaderProgram program("VertexShader.v", "FragmentShader.f");
 	
-
-
 
 	bool quit = false;
 
@@ -224,7 +203,7 @@ int main()
 			
 		}
 
-		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
